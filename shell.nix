3c5683527737bb2839158d3f974119f90a8222a4
OfflineBot <FlintstoneLG@gmail.com>
2024-10-16 22:17:44 +0200

    shell

--- shell.nix
+++ shell.nix
@@ -5,9 +5,11 @@ pkgs.mkShell {
         python3Packages.pip
         python3Packages.torch
         python3Packages.numpy
+        python3Packages.pillow
+        python3Packages.matplotlib
     ];
 
     shellHook = ''
-        export PS1="(env) \[\033[0;34m\]\w> \[\033[0;37m\]"
+        export PS1="(shell) \[\033[0;34m\]\w> \[\033[0;37m\]"
     '';
 }
