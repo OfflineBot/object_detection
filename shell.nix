e7c922430e13d2ea0b6f094fb5a27bf026148313
OfflineBot <Flintstonelg@gmail.com>
2024-10-17 08:58:13 +0200

    obj_detection

--- shell.nix
+++ shell.nix
@@ -2,14 +2,16 @@
 pkgs.mkShell {
     buildInputs = with pkgs; [
         python3
-        python3Packages.pip
-        python3Packages.torch
-        python3Packages.numpy
-        python3Packages.pillow
-        python3Packages.matplotlib
-    ];
+    ] ++ (with python3Packages; [
+        pip
+        torch
+        numpy
+        pillow
+        matplotlib
+    ]);
 
     shellHook = ''
         export PS1="(shell) \[\033[0;34m\]\w> \[\033[0;37m\]"
     '';
 }
+
